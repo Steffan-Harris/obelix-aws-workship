06f8c298b8a5a5e347f841e6758029a02c5cad41
Steffan Harris <steffan.harris@vodafone.com>
2022-02-18 15:47:32 +0000

    Commit everything

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ exports.handler = async (event) => {
   // TODO implement
   const response = {
     statusCode: 200,
-    body: JSON.stringify('Hello from Steffan!'),
+    body: JSON.stringify('Hello from Steffan\'s Github!'),
   };
   return response;
 };
